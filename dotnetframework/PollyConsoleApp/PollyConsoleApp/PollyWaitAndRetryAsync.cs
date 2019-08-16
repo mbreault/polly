fe3a884ecc1953a43180532e3a311d8ce9e9f6e5
Mike Breault <mikeb@mcrtechnologies.com>
2019-08-16 09:15:16 -0400

    polly examples

---
+++ dotnetframework/PollyConsoleApp/PollyConsoleApp/PollyWaitAndRetryAsync.cs
@@ -0,0 +1,70 @@
+﻿using Polly;
+using Polly.Retry;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+// Based on code from https://github.com/App-vNext/Polly-Samples
+
+namespace PollyConsoleApp
+{
+    public class PollyWaitAndRetryAsync
+    {
+        private int _retries;
+        private int _totalRequests;
+        private int _eventualSuccesses;
+        private int _eventualFailures;
+        private AsyncRetryPolicy _policy;
+
+        public PollyWaitAndRetryAsync()
+        {
+            _retries = 0;
+            _totalRequests = 0;
+            _eventualSuccesses = 0;
+            _eventualFailures = 0;
+
+            Random jitterer = new Random();
+
+            _policy = Policy.Handle<Exception>().WaitAndRetryAsync(
+                      retryCount: 3,
+                      sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                      onRetry: (exception, calculatedWaitDuration) => // Capture some info for logging!
+                      {
+                          Console.WriteLine("Policy logging: " + exception.Message);
+                          _retries++;
+                      });
+        }
+
+        public async Task ExecuteAsync(CancellationToken cancellationToken, string requestURI)
+        {
+            using (var client = new HttpClient())
+            {
+
+                _totalRequests++;
+
+                try
+                {
+                    // Retry the following call according to the policy 
+                    await _policy.ExecuteAsync(async token =>
+                    {
+                        // This code is executed within the Policy 
+
+                        // Make a request and get a response
+                        string msg = await client.GetStringAsync(requestURI);
+
+                        // Display the response message on the console
+                        Console.WriteLine("Response : " + msg);
+                        _eventualSuccesses++;
+                    }, cancellationToken);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Request " + _totalRequests + " eventually failed with: " + e.Message);
+                    _eventualFailures++;
+                }
+            }
+        }
+    }
+}
+
---
+++ dotnetframework/PollyConsoleApp/PollyConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using Polly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PollyConsoleApp
+{
+    class Program
+    {
+        private static int totalRequests;
+        private static int eventualSuccesses;
+        private static int retries;
+        private static int eventualFailures;
+
+        static void Main(string[] args)
+        {
+
+            PollyWaitAndRetryAsync pollyWaitAndRetryAsync = new PollyWaitAndRetryAsync();
+            string url = "http://http://asdasfadfdf.com/";
+            pollyWaitAndRetryAsync.ExecuteAsync(new CancellationToken(), url).GetAwaiter().GetResult();
+        }
+    }
+}
